2239a221425895d650cae8f2ac36342700c832a1
Stu Fisher <stu@q3f.org>
2018-08-07 19:33:09 +0200

    update sw cache

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js')
 
 if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
+  	console.log(`Yay! Workbox is loaded 🎉`)
 
-  workbox.precaching.precacheAndRoute([
+  	workbox.precaching.precacheAndRoute([
   {
     "url": "assets/css/stylesheets/main.css",
-    "revision": "5fa3cb7031cddb16cb9c18a30b704e2f"
+    "revision": "6587a6a6c6a92602fde06620b20cc7aa"
   },
   {
     "url": "assets/fonts/DroidSans-Bold.woff",
@@ -33,6 +33,14 @@ if (workbox) {
     "revision": "afa394bb585f9b5916bb67d1582a899e"
   },
   {
+    "url": "assets/images/icons/qd-128.png",
+    "revision": "58748bc5bac86ef2b88e4f9a77ed21f7"
+  },
+  {
+    "url": "assets/images/icons/qd-144.png",
+    "revision": "facd79e5e10dd0f499d96bc27e8d5b63"
+  },
+  {
     "url": "assets/images/icons/qd-152.png",
     "revision": "c49475fb404ad2578ba50b6fcc1edc94"
   },
@@ -43,6 +51,14 @@ if (workbox) {
   {
     "url": "assets/images/icons/qd-180.png",
     "revision": "80f566bb9bc2b7c437b3e933e6257dae"
+  },
+  {
+    "url": "assets/images/icons/qd-192.png",
+    "revision": "ed693a291317d55b606c6d32d02b80cd"
+  },
+  {
+    "url": "assets/images/icons/qd-256.png",
+    "revision": "45364b1ef0afcaecbd2b2fb31bf0a9b8"
   },
   {
     "url": "assets/images/ios/home.png",
@@ -61,12 +77,36 @@ if (workbox) {
     "revision": "179602e000e84306ce4652e8e4e1e10d"
   },
   {
+    "url": "assets/images/splash/splash-1125x2436.png",
+    "revision": "e0263908567b4d459975aff58cadce1b"
+  },
+  {
+    "url": "assets/images/splash/splash-1242x2208.png",
+    "revision": "22b653e5b969d06252911acd29a85791"
+  },
+  {
+    "url": "assets/images/splash/splash-1536x2048.png",
+    "revision": "a3cc19dc7046757b0ce2554dc7f01736"
+  },
+  {
+    "url": "assets/images/splash/splash-1668x2224.png",
+    "revision": "897b6589956e48b123f3545bb32dd876"
+  },
+  {
+    "url": "assets/images/splash/splash-2048x2732.png",
+    "revision": "41674bf5c6d65103e0f4506a07ef9ad2"
+  },
+  {
     "url": "assets/images/splash/splash-640x1136.png",
-    "revision": "cbcf6d6b3e893792152a876feee48eda"
+    "revision": "6cd57d0db4f7ec74a7cc928b6ec5dc4a"
+  },
+  {
+    "url": "assets/images/splash/splash-750x1334.png",
+    "revision": "28e4b3f3f7a29e5f8169e0f12f5f76ef"
   },
   {
     "url": "index.html",
-    "revision": "4fdcd5ccb30707522ca55dc48b39cff6"
+    "revision": "b8d55a1910ed21cb099bb5e5dc754317"
   },
   {
     "url": "dist/vendor/requirejs/require.js",
@@ -82,7 +122,7 @@ if (workbox) {
   },
   {
     "url": "dist/main.js",
-    "revision": "e3196e1cfd4a3723f84d9eb9d06ad536"
+    "revision": "1adc643d271d926eb95acb3263658b77"
   },
   {
     "url": "dist/tables/elements.json",
@@ -94,14 +134,19 @@ if (workbox) {
   },
   {
     "url": "dist/tables/statuses.json",
-    "revision": "33b4865ccf28127cfa2fff824f83c8b3"
+    "revision": "403ef2e8b19fe6540e63945e98a875d3"
   },
   {
     "url": "js/worker.js",
     "revision": "0ef7127dc9a11dcb7df255d826dd8f56"
   }
 ]);
+  	workbox.googleAnalytics.initialize({
+	  	parameterOverrides: {
+	    	cd1: 'offline',
+	  	},
+	})
 
 } else {
-  console.log(`Boo! Workbox didn't load 😬`);
+  	console.log(`Boo! Workbox didn't load 😬`)
 }
