acb039a5887d9af7192ce5992517444bbeb4bed7
Stu Fisher <stu@q3f.org>
2018-08-07 11:42:40 +0200

    add status model

--- js/collections/statuses.js
+++ js/collections/statuses.js
@@ -1,6 +1,8 @@
-define(['backbone', 'utils/kvcollection'], function(Backbone, KVCollection) {
+define(['backbone', 'models/status', 'utils/kvcollection'], function(Backbone, Status, KVCollection) {
 
     return Backbone.Collection.extend(_.extend({}, KVCollection, {
+    	model: Status,
+    	
         keyAttribute: 'facility',
         valueAttribute: 'facility',
 
