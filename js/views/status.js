d6b613d92e64dd2e92cbd4f46b031f52a83d0caa
Stu Fisher <stu@q3f.org>
2018-08-07 12:21:57 +0200

    add default facility, add offline message

--- js/views/status.js
+++ js/views/status.js
@@ -1,8 +1,11 @@
 define(['backbone.marionette',
+    'utils/indexdb',
+    'models/status',
     'collections/statuses',
     'json!tables/statuses.json',
     'tpl!templates/statuses.html'], function(Marionette, 
-    Statuses, statuses,
+    Store,
+    Status, Statuses, statuses,
     template) {
 
 
@@ -13,11 +16,15 @@ define(['backbone.marionette',
         ui: {
             fac: 'select[name=facility]',
             img: 'img.status',
+            offline: '.offline',
         },
 
         events: {
             'change @ui.fac': 'updateFacility',
+            'click button[name=default]': 'saveDefault',
         },
+
+
 
 
         updateFacility: function() {
@@ -50,6 +57,47 @@ define(['backbone.marionette',
         
         initialize: function(options) {
             this.statuses = new Statuses(statuses)
+
+            this.store = new Store()
+            this.customfacilities = new Statuses()
+            
+            this.listenTo(app, 'status', this.updateStatus)
+        },
+
+        updateStatus: function() {
+            app.status ? this.ui.offline.removeClass('show') : this.ui.offline.addClass('show')
+        },
+
+
+        loadCustom: function() {
+            var self = this
+            this.store.get({ store: 'facilities' }).then(function(custom) {
+                console.log('got custom facilites', custom)
+                self.customfacilities.reset(custom)
+                self.setDefault()
+            })
+        },
+
+        setDefault: function() {
+            var d = this.customfacilities.findWhere({ name: 'default' })
+            if (d) {
+                this.ui.fac.val(d.get('value')).trigger('change')
+            }
+        },
+
+
+        saveDefault: function(e) {
+            e.preventDefault()
+            var d = this.customfacilities.findWhere({ name: 'default' })
+            if (!d) {
+                d = new Status({ name: 'default' })
+                this.customfacilities.add(d)
+            }
+            d.set({ value: this.ui.fac.val() })
+
+            this.store.save({ store: 'facilities', objects: this.customfacilities.toJSON() }).then(function() {
+                console.log('saved default facility')
+            })
         },
 
 
@@ -57,6 +105,7 @@ define(['backbone.marionette',
             this.ui.img[0].onload = this.showFacility.bind(this)
             this.populateFacilites()
             this.updateFacility()
+            this.loadCustom()
         }
     })
 
